1433012d5fd33609a8f07678c0fe30876855d7e0
Rosto <45299268+harryminaticikk@users.noreply.github.com>
2021-11-21 16:27:02 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,21 @@
-yazcam
+const Discord = require('discord.js');
+
+exports.run = async(client, message, args) => {
+ 
+message.channel.send(`**Pong!** + ${client.ws.ping} +`)
+
+};
+
+exports.conf = {
+  enabled : true,
+  guildOnly : false,
+  aliases : ['pong'],
+  permLevel : 0,
+  category : ''
+};
+
+exports.help = {
+  name : 'ping pong',
+  description : 'Botun pingini gösterir.',
+  usage : 'ping'
+};
